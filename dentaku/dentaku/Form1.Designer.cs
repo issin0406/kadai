33aa07924257b255af91b63e06ce6b2379bc5816
issin <issin406@gmail.com>
2020-08-18 13:10:08 +0900

    saisyo

---
+++ dentaku/dentaku/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace dentaku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.allclear = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.divid = new System.Windows.Forms.Button();
+            this.times = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(211, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(306, 185);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 44);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(116, 235);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 44);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(211, 235);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 44);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(306, 235);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(89, 44);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(116, 285);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(89, 44);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(211, 285);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(89, 44);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(306, 285);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(89, 44);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(116, 335);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(89, 44);
+            this.button0.TabIndex = 9;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(116, 135);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(89, 44);
+            this.clear.TabIndex = 10;
+            this.clear.Text = "C";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // allclear
+            // 
+            this.allclear.Location = new System.Drawing.Point(211, 135);
+            this.allclear.Name = "allclear";
+            this.allclear.Size = new System.Drawing.Size(89, 44);
+            this.allclear.TabIndex = 11;
+            this.allclear.Text = "AC";
+            this.allclear.UseVisualStyleBackColor = true;
+            this.allclear.Click += new System.EventHandler(this.allclear_Click);
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(306, 335);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(89, 44);
+            this.result.TabIndex = 12;
+            this.result.Text = "＝";
+            this.result.UseVisualStyleBackColor = true;
+            this.result.Click += new System.EventHandler(this.result_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(401, 185);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(89, 44);
+            this.plus.TabIndex = 13;
+            this.plus.Text = "＋";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(401, 235);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(89, 44);
+            this.minus.TabIndex = 14;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // divid
+            // 
+            this.divid.Location = new System.Drawing.Point(401, 335);
+            this.divid.Name = "divid";
+            this.divid.Size = new System.Drawing.Size(89, 44);
+            this.divid.TabIndex = 15;
+            this.divid.Text = "÷";
+            this.divid.UseVisualStyleBackColor = true;
+            this.divid.Click += new System.EventHandler(this.divid_Click);
+            // 
+            // times
+            // 
+            this.times.Location = new System.Drawing.Point(401, 285);
+            this.times.Name = "times";
+            this.times.Size = new System.Drawing.Size(89, 44);
+            this.times.TabIndex = 16;
+            this.times.Text = "×";
+            this.times.UseVisualStyleBackColor = true;
+            this.times.Click += new System.EventHandler(this.times_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(114, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 65);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(114, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(376, 37);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // dot
+            // 
+            this.dot.Location = new System.Drawing.Point(212, 336);
+            this.dot.Name = "dot";
+            this.dot.Size = new System.Drawing.Size(88, 43);
+            this.dot.TabIndex = 19;
+            this.dot.Text = ".";
+            this.dot.UseVisualStyleBackColor = true;
+            this.dot.Click += new System.EventHandler(this.dot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 450);
+            this.Controls.Add(this.dot);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.times);
+            this.Controls.Add(this.divid);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.allclear);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button allclear;
+        private System.Windows.Forms.Button result;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button divid;
+        private System.Windows.Forms.Button times;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button dot;
+    }
+}
+
---
+++ dentaku/dentaku/Form1.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dentaku
+{
+    public partial class Form1 : Form
+    {
+        string message1 = "";
+        string message2 = "";
+        string message3 = "";
+        int flag = 0;
+        float a;
+        float b;
+        float c;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(flag == 0)
+            {
+                message1 = message1 + "1";
+                label1.Text = message1;
+            }
+            else
+            {
+                message2 = message2 + "1";
+                label1.Text = message2;
+            }
+            
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if (flag == 0)
+            {
+                message1 = message1 + "0";
+                label1.Text = message1;
+            }
+            else
+            {
+                message2 = message2 + "0";
+                label1.Text = message2;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (flag == 0)
+            {
+                message1 = message1 + "2";
+                label1.Text = message1;
+            }
+            else
+            {
+                message2 = message2 + "2";
+                label1.Text = message2;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (flag == 0)
+            {
+                message1 = message1 + "3";
+                label1.Text = message1;
+            }
+            else
+            {
+                message2 = message2 + "3";
+                label1.Text = message2;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (flag == 0)
+            {
+                message1 = message1 + "4";
+                label1.Text = message1;
+            }
+            else
+            {
+                message2 = message2 + "4";
+                label1.Text = message2;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (flag == 0)
+            {
+                message1 = message1 + "5";
+                label1.Text = message1;
+            }
+            else
+            {
+                message2 = message2 + "5";
+                label1.Text = message2;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (flag == 0)
+            {
+                message1 = message1 + "6";
+                label1.Text = message1;
+            }
+            else
+            {
+                message2 = message2 + "6";
+                label1.Text = message2;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (flag == 0)
+            {
+                message1 = message1 + "7";
+                label1.Text = message1;
+            }
+            else
+            {
+                message2 = message2 + "7";
+                label1.Text = message2;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (flag == 0)
+            {
+                message1 = message1 + "8";
+                label1.Text = message1;
+            }
+            else
+            {
+                message2 = message2 + "8";
+                label1.Text = message2;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (flag == 0)
+            {
+                message1 = message1 + "9";
+                label1.Text = message1;
+            }
+            else
+            {
+                message2 = message2 + "9";
+                label1.Text = message2;
+            }
+        }
+
+        private void result_Click(object sender, EventArgs e)
+        {
+            if(flag == 0)
+            {
+
+            }
+            else
+            {
+                message3 = message3 + message2 + " = ";
+                label2.Text = message3;
+                b = float.Parse(message2);
+                if (flag == 1)
+                {
+                    c = a + b;
+                }else if (flag == 2)
+                {
+                    c = a - b;
+                }else if(flag == 3)
+                {
+                    c = a * b;
+                }
+                else
+                {
+                    c = a / b;
+                }
+                label1.Text = c.ToString();
+                message1 = c.ToString();
+                message2 = "";
+                flag = 0;
+            }
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            if(flag == 1 || flag == 2 || flag == 3 || flag == 4)
+            {
+               /* b = float.Parse(message2);
+                if (flag == 1)
+                {
+                    c = a + b;
+                }
+                else if (flag == 2)
+                {
+                    c = a - b;
+                }
+                else if (flag == 3)
+                {
+                    c = a * b;
+                }
+                else
+                {
+                    c = a / b;
+                }
+                label2.Text = c.ToString() + "+";
+                label1.Text = "0";
+                message1 = c.ToString();
+                message2 = "";
+                c = a;
+                flag = 1; */
+            }
+            else
+            {
+                a = float.Parse(message1);
+                message3 = message1 + " + ";
+                message1 = "";
+                label2.Text = message3;
+                label1.Text = "0";
+                flag = 1;
+            }
+           
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            if (flag == 1 || flag == 2 || flag == 3 || flag == 4)
+            {
+               /* b = float.Parse(message2);
+                if (flag == 1)
+                {
+                    c = a + b;
+                }
+                else if (flag == 2)
+                {
+                    c = a - b;
+                }
+                else if (flag == 3)
+                {
+                    c = a * b;
+                }
+                else
+                {
+                    c = a / b;
+                }
+                label2.Text = c.ToString() + "-";
+                label1.Text = "0";
+                message1 = c.ToString();
+                message2 = "";
+                c = a;
+                flag = 2;*/
+            }
+            else
+            {
+                a = float.Parse(message1);
+                message3 = message1 + " - ";
+                message1 = "";
+                label2.Text = message3;
+                label1.Text = "0";
+                flag = 2;
+            }
+        }
+
+        private void times_Click(object sender, EventArgs e)
+        {
+            if (flag == 1 || flag == 2 || flag == 3 || flag == 4)
+            {
+                /*b = float.Parse(message2);
+                if (flag == 1)
+                {
+                    c = a + b;
+                }
+                else if (flag == 2)
+                {
+                    c = a - b;
+                }
+                else if (flag == 3)
+                {
+                    c = a * b;
+                }
+                else
+                {
+                    c = a / b;
+                }
+                label2.Text = c.ToString() + "×";
+                label1.Text = "0";
+                message1 = c.ToString();
+                message2 = "";
+                c = a;
+                flag = 3;*/
+            }
+            else
+            {
+                a = float.Parse(message1);
+                message3 = message1 + " × ";
+                message1 = "";
+                label2.Text = message3;
+                label1.Text = "0";
+                flag = 3;
+            }
+        }
+
+        private void divid_Click(object sender, EventArgs e)
+        {
+            if (flag == 1 || flag == 2 || flag == 3 || flag == 4)
+            {
+               /* b = float.Parse(message2);
+                if (flag == 1)
+                {
+                    c = a + b;
+                }
+                else if (flag == 2)
+                {
+                    c = a - b;
+                }
+                else if (flag == 3)
+                {
+                    c = a * b;
+                }
+                else
+                {
+                    c = a / b;
+                }
+                label2.Text = c.ToString() + "÷";
+                label1.Text = "0";
+                message1 = c.ToString();
+                message2 = "";
+                c = a;
+                flag = 4;*/
+            }
+            else
+            {
+                a = float.Parse(message1);
+                message3 = message1 + " ÷ ";
+                message1 = "";
+                label2.Text = message3;
+                label1.Text = "0";
+                flag = 4;
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            message1 = "";
+            message2 = "";
+        }
+
+        private void allclear_Click(object sender, EventArgs e)
+        {
+            label1.Text = "0";
+            label2.Text = "0";
+            message1 = "";
+            message2 = "";
+            message3 = "";
+            flag = 0;
+            a = 0;
+            b = 0;
+            c = 0;
+        }
+
+        private void dot_Click(object sender, EventArgs e)
+        {
+            if (flag == 0)
+            {
+                message1 = message1 + ".";
+                label1.Text = message1;
+            }
+            else
+            {
+                message2 = message2 + ".";
+                label1.Text = message1 + message2;
+            }
+        }
+    }
+}
